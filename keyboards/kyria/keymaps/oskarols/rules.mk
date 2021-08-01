5699b8d6a5c87e236755a36b6304c6fb97efc636
Oskar Olsson <oskar.ols@gmail.com>
2021-08-01 17:03:59 +0200

    enable mouse pressing

--- keyboards/kyria/keymaps/oskarols/rules.mk
+++ keyboards/kyria/keymaps/oskarols/rules.mk
@@ -3,3 +3,5 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
 COMBO_ENABLE = yes 		   # Enable chording/combos
 LEADER_ENABLE = yes        # Enable leader keys
+
+MOUSEKEY_ENABLE = yes
